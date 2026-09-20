a30a692acdf36b17a7211d1176984bab86d531f8
deeivihh <deeivihh@hotmail.com>
2026-09-21 00:30:26 +0200

    Minor fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     images: "/og/og-image.jpg",
     title: "David Lahoz",
+    site: "davidlh.com",
     description: "David Lahoz is a frontend developer and marketing student working across digital products, data, AI and brand experiences",
   },
   icons: {
